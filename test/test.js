cc00f0dfc8e97c22fb15876ccf3ae9ca1b28e59b
alexchasejones <Alex.Jones@intersection.com>
2017-08-10 11:19:10 -0400

    initial tests

--- test/test.js
+++ test/test.js
@@ -1,5 +1,19 @@
 import test from 'ava';
+import { baseURL} from '../src/scripts/config';
+import TEST_DATA as data from '../src/scripts/test-data';
 
 test('ava test example', t => {
-	t.pass();
+	t.pass(); 
 });
+
+test('baseUrl exists', t => {
+	if (baseURL) {
+		t.pass();
+	} else {
+		t.fail();
+	}
+});
+
+test('Test data contains Latitude', t => {
+	if (TEST_) {}
+})
