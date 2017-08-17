5a5c5865e2243d88c675f2018628cf4bb9ee295c
alexchasejones <Alex.Jones@intersection.com>
2017-08-17 11:48:31 -0400

    removing useless tests

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 import test from 'ava';
-import { baseURL, ad_id } from '../src/scripts/config';
-import TEST_DATA from '../src/scripts/test-data';
+import { baseURL } from '../src/scripts/config';
+import View from '../src/scripts/view';
 
 test('ava test example', t => {
 	t.pass(); 
@@ -14,37 +14,11 @@ test('baseUrl exists', t => {
 	}
 });
 
-test('Test data contains latitude', t => {
-	if (TEST_DATA[0].latitude) {
+test('If no URL is specified, throw an error', t => {
+	const view = new View();
+	if(view.request() === false) {
 		t.pass();
 	} else {
 		t.fail();
 	}
-});
-
-test('Test data contains longitude', t => {
-	if (TEST_DATA[0].longitude) {
-		t.pass();
-	} else {
-		t.fail();
-	}
-});
-
-test('Test data contains impressions per 15 seconds', t => {
-	if (TEST_DATA[0].impressions_15_sec) {
-		t.pass();
-	} else {
-		t.fail();
-	}
-});
-
-test('Test data contains a correctly formatted partner venue id', t => {
-	if (TEST_DATA[0]._index) {
-		const regex = /(MN|BK|QU|SI|BX)-\d\d-\d\d\d\d\d\d-(L|R)/gi
-		if (TEST_DATA[0]._index.match(regex)) {
-			t.pass();
-			return
-		}
-	}
-	t.fail();
 });
