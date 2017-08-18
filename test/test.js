a060e54508a8cd7612494254dbd2fa7137c88dc0
alexchasejones <Alex.Jones@intersection.com>
2017-08-18 11:49:55 -0400

    added impression validator method tests

--- test/test.js
+++ test/test.js
@@ -1,4 +1,7 @@
+/* eslint-disable */
 import test from 'ava';
+import View from '../src/scripts/view';
+
 const nock = require('nock');
 const request = require('request');
 
@@ -11,7 +14,7 @@ nock.disableNetConnect();
 let body = {};
 let serverMock = nock('http://pda-server.dev.linksvc.com').post('/play/ad?site_id=&imp_x=&lat=&lon=', {}).reply(204);
 
-test('pda server /play endpoint returns 204 response with no content', t => {
+test('PDA Server \'/play\' endpoint returns 204 response with no content', t => {
 	request({
 		method: 'POST',
 		uri: 'http://pda-server.dev.linksvc.com/play/ad?site_id=&imp_x=&lat=&lon=',
@@ -28,4 +31,13 @@ test('pda server /play endpoint returns 204 response with no content', t => {
 			}
 		}
 	});	
+})
+
+test('Impression validator tests', t => {
+	const view = new View();
+	t.is(view.imp(10), 10, 'impressions rounded up incorrectly');
+	t.is(view.imp('test'), 0, 'wrong data type not handled correctly');
+	t.is(view.imp(undefined), 0, 'undefined not handled correctly');
+	t.is(view.imp(null), 0, 'null not hanlded correctly');
+	t.pass();
 })
