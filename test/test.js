83ed43cd3bfb715cc59c18c70bc33ab4a2bd3bf4
alexchasejones <Alex.Jones@intersection.com>
2017-08-21 09:53:22 -0400

    updating test folder structure and renaming impression test

--- test/test.js
+++ test/test.js
@@ -35,7 +35,7 @@ test('PDA Server \'/play\' endpoint returns 204 response with no content', t => 
 
 test('Impression validator tests', t => {
 	const view = new View();
-	t.is(view.imp(10), 10, 'impressions rounded up incorrectly');
+	t.is(view.imp(10), 10, 'impressions returning incorrectly');
 	t.is(view.imp('test'), 0, 'wrong data type not handled correctly');
 	t.is(view.imp(undefined), 0, 'undefined not handled correctly');
 	t.is(view.imp(null), 0, 'null not hanlded correctly');
